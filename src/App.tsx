8c2e09d4380dab09a999d20ace8b4b2c194ccd3b
Nadezhda Kireyenka <70837252+NadieKir@users.noreply.github.com>
2022-12-18 00:37:59 +0300

    Add animation to header links

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import styles from './App.module.scss';
+import { Header } from './components/';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <div className={styles['app-container']}>
         <Routes>
           <Route index path="/meetups" element={<div>Meetups</div>} />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
 
 import styles from './Header.module.scss';
 import logo from 'assets/images/logo.svg';
 
 export const Header = (): JSX.Element => {
+  const getLinkClassName = ({ isActive }: { isActive: boolean }) =>
+    isActive
+      ? classNames(styles['nav-link'], styles['active'])
+      : styles['nav-link'];
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <img src={logo} alt="Логотип" height={'45px'} />
         <nav className={styles.nav}>
-          <NavLink to="/meetups" className={styles['nav-link']}>
+          <NavLink to="/meetups" className={getLinkClassName}>
             Митапы
           </NavLink>
-          <NavLink to="/news" className={styles['nav-link']}>
+          <NavLink to="/news" className={getLinkClassName}>
             Новости
           </NavLink>
         </nav>
