8ef82b976603394ea949d5b08ea7450f310fd5ef
Nadezhda Kireyenka <70837252+NadieKir@users.noreply.github.com>
2022-12-17 23:18:59 +0300

    Setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import styles from './App.module.scss';
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <div className={styles['app-container']}>
+        <Routes>
+          <Route index path="/meetups" element={<div>Meetups</div>} />
+          <Route path="/news" element={<div>News</div>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
