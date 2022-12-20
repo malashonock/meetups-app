88898a4decbba16153640636cbacb0b36e394d61
Nadezhda Kireyenka <70837252+NadieKir@users.noreply.github.com>
2022-12-20 18:12:01 +0300

    Fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ function App() {
       <Header />
       <div className={styles['app-container']}>
         <Routes>
-          <Route index path="/meetups" element={<div>Meetups</div>} />
-          <Route path="/news" element={<div>News</div>} />
           <Route path="/" element={<Navigate replace to="/meetups" />} />
+          <Route path="/meetups" element={<div>Meetups</div>} />
+          <Route path="/news" element={<div>News</div>} />
           <Route path="*" element={<div>404</div>} />
         </Routes>
       </div>
