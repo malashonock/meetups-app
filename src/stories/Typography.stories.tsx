c1217a856a297c04af9060476f99b5f046c7efae
Yan Malashonak <malashonock@gmail.com>
2022-12-21 17:36:30 +0300

    refactor: Typography stories

--- src/stories/Typography.stories.tsx
+++ src/stories/Typography.stories.tsx
@@ -1,9 +1,45 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Typography } from 'components';
 
+const typographyUtilities: string[] = [
+  'ff-1',
+  'ff-2',
+  'fw-regular',
+  'fw-medium',
+  'fw-bold',
+  'fs-xs',
+  'fs-s',
+  'fs-m',
+  'fs-l',
+  'fs-xl',
+  'lh-xs',
+  'lh-s',
+  'lh-m',
+  'lh-l',
+  'lh-xl',
+  'lh-xxl',
+  'ls-0',
+  'ls-1',
+  'fc-black',
+  'fc-gray-01',
+  'fc-gray-02',
+  'fc-gray-03',
+  'fc-white',
+  'fc-purple',
+  'ta-left',
+  'ta-center',
+  'ta-right',
+];
+
 export default {
   title: 'Components/Typography',
   component: Typography,
+  argTypes: {
+    className: {
+      options: typographyUtilities,
+      control: { type: 'check' },
+    },
+  },
   args: {
     children: 'Sample text',
     style: {},
@@ -14,97 +50,17 @@ const Template: ComponentStory<typeof Typography> = (args) => (
   <Typography {...args} />
 );
 
-export const BodyS = Template.bind({});
-BodyS.args = {
-  variant: 'body--s',
+export const HeadingTypography = Template.bind({});
+HeadingTypography.args = {
+  variant: 'heading',
 };
 
-export const BodyXS = Template.bind({});
-BodyXS.args = {
-  variant: 'body--xs',
+export const ParagraphTypography = Template.bind({});
+ParagraphTypography.args = {
+  variant: 'paragraph',
 };
 
-export const BtnTextDefault = Template.bind({});
-BtnTextDefault.args = {
-  variant: 'btn-text--default',
-};
-
-export const BtnTextPrimary = Template.bind({});
-BtnTextPrimary.args = {
-  variant: 'btn-text--primary',
-};
-
-export const BtnTextSecondary = Template.bind({});
-BtnTextSecondary.args = {
-  variant: 'btn-text--secondary',
-};
-
-export const H1F1 = Template.bind({});
-H1F1.args = {
-  variant: 'h1--f1',
-};
-
-export const H2F1 = Template.bind({});
-H2F1.args = {
-  variant: 'h2--f1',
-};
-
-export const H3F1 = Template.bind({});
-H3F1.args = {
-  variant: 'h3--f1',
-};
-
-export const H4F1 = Template.bind({});
-H4F1.args = {
-  variant: 'h4--f1',
-};
-
-export const H2F2 = Template.bind({});
-H2F2.args = {
-  variant: 'h2--f2',
-};
-
-export const H3F2 = Template.bind({});
-H3F2.args = {
-  variant: 'h3--f2',
-};
-
-export const Label = Template.bind({});
-Label.args = {
-  variant: 'label',
-};
-
-export const ParagraphC1 = Template.bind({});
-ParagraphC1.args = {
-  variant: 'paragraph--c1',
-};
-
-export const ParagraphC2 = Template.bind({});
-ParagraphC2.args = {
-  variant: 'paragraph--c2',
-};
-
-export const ParagraphC3 = Template.bind({});
-ParagraphC3.args = {
-  variant: 'paragraph--c3',
-};
-
-export const PlaceholderActive = Template.bind({});
-PlaceholderActive.args = {
-  variant: 'placeholder--active',
-};
-
-export const PlaceholderDefault = Template.bind({});
-PlaceholderDefault.args = {
-  variant: 'placeholder--default',
-};
-
-export const PlaceholderFocus = Template.bind({});
-PlaceholderFocus.args = {
-  variant: 'placeholder--focus',
-};
-
-export const Subtitle = Template.bind({});
-Subtitle.args = {
-  variant: 'subtitle',
+export const OtherTypography = Template.bind({});
+OtherTypography.args = {
+  variant: 'other',
 };
