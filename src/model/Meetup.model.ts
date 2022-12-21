a1cc52a1466eb83afa040d6f6601758d029814df
Yan Malashonak <malashonock@gmail.com>
2022-12-21 10:55:24 +0300

    feat: implement Meetup model

---
+++ src/model/Meetup.model.ts
@@ -0,0 +1,22 @@
+import { ShortUser } from 'model';
+
+export enum MeetupStatus {
+  REQUEST = 'request',
+  DRAFT = 'draft',
+  CONFIRMED = 'confirmed',
+}
+
+export interface Meetup {
+  id: string;
+  modified: string; // DateTime string
+  start?: string; // DateTime string
+  finish?: string; // DateTime string
+  author: ShortUser;
+  speakers: ShortUser[];
+  subject: string;
+  excerpt?: string;
+  place?: string;
+  goCount: number;
+  status: MeetupStatus;
+  isOver: boolean;
+}
