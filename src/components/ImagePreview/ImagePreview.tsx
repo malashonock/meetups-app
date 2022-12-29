5338472aebe51fe7749dd35ed5dd067137e84384
Yan Malashonak <yan.malashonak@gmail.com>
2022-12-29 21:50:28 +0300

    feat: implement ImagePreview component

--- src/components/ImagePreview/ImagePreview.tsx
+++ src/components/ImagePreview/ImagePreview.tsx
@@ -1,9 +1,52 @@
+import { FileWithPath } from 'react-dropzone';
+import { Typography, TypographyComponent } from 'components';
+import { ReactComponent as ThumbnailIcon } from './assets/thumbnail.svg';
+import { ReactComponent as CloseIcon } from './assets/close.svg';
+import { FileWithUrl } from 'types';
+import { convertBytesToMb } from 'helpers';
 import styles from './ImagePreview.module.scss';
 
 interface ImagePreviewProps {
-  image: File;
+  image: FileWithUrl;
+  onClear: () => void;
 }
 
-export const ImagePreview = ({ image }: ImagePreviewProps): JSX.Element => {
-  return <></>;
+export const ImagePreview = ({
+  image,
+  onClear,
+}: ImagePreviewProps): JSX.Element => {
+  const { name, size, url } = image;
+
+  const handleClear = (): void => {
+    onClear();
+  };
+
+  return (
+    <div className={styles.preview}>
+      <figure className={styles.thumbnail}>
+        {url ? (
+          <img src={url} alt="Image thumbnail" />
+        ) : (
+          <ThumbnailIcon className={styles.placeholder} />
+        )}
+      </figure>
+      <div className={styles.info}>
+        <Typography
+          component={TypographyComponent.Heading4}
+          className={styles.fileName}
+        >
+          {name}
+        </Typography>
+        <Typography
+          component={TypographyComponent.Paragraph}
+          className={styles.fileSize}
+        >
+          File size: {convertBytesToMb(size).toFixedIfAny(1)} Mb
+        </Typography>
+      </div>
+      <button className={styles.clearBtn} onClick={handleClear}>
+        <CloseIcon className={styles.clearIcon} />
+      </button>
+    </div>
+  );
 };
