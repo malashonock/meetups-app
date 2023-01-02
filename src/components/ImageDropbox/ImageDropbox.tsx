df91c83012299f2ac017150b57bfe43d71d50601
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-03 01:38:12 +0300

    feat: add file rejection error messages

--- src/components/ImageDropbox/ImageDropbox.tsx
+++ src/components/ImageDropbox/ImageDropbox.tsx
@@ -1,20 +1,25 @@
-import { FileWithUrl } from 'types';
+import { useState } from 'react';
 import classNames from 'classnames';
-import { useDropzone } from 'react-dropzone';
+import { FileError, FileRejection, useDropzone } from 'react-dropzone';
 import { Typography, TypographyComponent } from 'components';
 import { ReactComponent as UploadIcon } from './upload.svg';
 import { getFileSizeString } from 'helpers';
+import { FileWithUrl } from 'types';
 import styles from './ImageDropbox.module.scss';
 
 const ACCEPT_FORMATS = ['.jpg', '.jpeg', '.png'];
 
 const MAX_FILESIZE = 10_000_000; // bytes
 
+const ERRORS_TIMEOUT = 3_000; // milliseconds
+
 interface ImageDropboxProps {
   onDrop: (image: FileWithUrl) => void;
 }
 
 export const ImageDropbox = ({ onDrop }: ImageDropboxProps): JSX.Element => {
+  const [errors, setErrors] = useState<FileError[]>([]);
+
   const acceptOptions = ACCEPT_FORMATS.reduce((formats, format) => {
     return {
       ...formats,
@@ -24,7 +29,7 @@ export const ImageDropbox = ({ onDrop }: ImageDropboxProps): JSX.Element => {
     };
   }, {});
 
-  const handleDrop = (acceptedFiles: File[]): void => {
+  const handleAcceptedDrop = (acceptedFiles: File[]): void => {
     if (acceptedFiles.length > 0) {
       const acceptedFile = acceptedFiles[0];
 
@@ -36,6 +41,15 @@ export const ImageDropbox = ({ onDrop }: ImageDropboxProps): JSX.Element => {
     }
   };
 
+  const handleRejectedDrop = (fileRejections: FileRejection[]): void => {
+    // https://github.com/react-dropzone/react-dropzone/pull/938
+    const errors = fileRejections.flatMap(
+      ({ errors }: FileRejection): FileError[] => errors,
+    );
+    setErrors(errors);
+    setTimeout(() => setErrors([]), ERRORS_TIMEOUT);
+  };
+
   const { getRootProps, getInputProps, isDragAccept, isDragReject, open } =
     useDropzone({
       accept: acceptOptions,
@@ -43,16 +57,24 @@ export const ImageDropbox = ({ onDrop }: ImageDropboxProps): JSX.Element => {
       noKeyboard: true,
       multiple: false,
       maxSize: MAX_FILESIZE,
-      onDrop: handleDrop,
+      onDropAccepted: handleAcceptedDrop,
+      onDropRejected: handleRejectedDrop,
     });
 
   const classList = classNames(
     styles.dropbox,
     isDragAccept ? styles.willAccept : '',
-    isDragReject ? styles.willReject : '',
+    isDragReject || errors.length > 0 ? styles.willReject : '',
   );
 
   const acceptFileExtensions = ACCEPT_FORMATS.join(' ');
+
+  const translateError = (error: string): string => {
+    return error
+      .replace('File type must be', 'Допустимые типы файлов:')
+      .replace('File is larger than', 'Размер файла превышает')
+      .replace('bytes', 'байт');
+  };
 
   return (
     <div className="container">
@@ -78,6 +100,22 @@ export const ImageDropbox = ({ onDrop }: ImageDropboxProps): JSX.Element => {
             Максимальный размер файла: {getFileSizeString(MAX_FILESIZE)}
           </Typography>
         </div>
+        {errors.length > 0 ? (
+          <ul className={styles.errors}>
+            {errors.map(({ code, message }: FileError): JSX.Element => {
+              return (
+                <li key={code}>
+                  <Typography
+                    component={TypographyComponent.Paragraph}
+                    className={styles.error}
+                  >
+                    {translateError(message)}
+                  </Typography>
+                </li>
+              );
+            })}
+          </ul>
+        ) : null}
       </div>
     </div>
   );
