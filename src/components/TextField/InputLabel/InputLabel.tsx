ca7820ca6de1708829fb379ddc91c1a36c048dbe
egorkalinin <yahor.kalinin@gmail.com>
2023-01-06 00:07:20 +0300

    Fix Typography, add nativeHtmlProps

--- src/components/TextField/InputLabel/InputLabel.tsx
+++ src/components/TextField/InputLabel/InputLabel.tsx
@@ -1,13 +1,20 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, HTMLAttributes } from 'react';
+import classNames from 'classnames';
 
-import { Typography } from 'components/Typography/Typography';
+import { Typography } from 'components';
+import { TypographyComponent } from 'components';
 
 import styles from './InputLabel.module.scss';
 
-export const InputLabel = ({ children }: PropsWithChildren): JSX.Element => {
-  return (
-    <Typography variant="heading" className={styles.label}>
-      {children}
-    </Typography>
-  );
-};
+export const InputLabel = ({
+  children,
+  ...nativeHtmlProps
+}: PropsWithChildren & HTMLAttributes<HTMLLabelElement>): JSX.Element => (
+  <Typography
+    component={TypographyComponent.Heading3}
+    className={classNames(nativeHtmlProps.className, styles.label)}
+    {...nativeHtmlProps}
+  >
+    {children}
+  </Typography>
+);
