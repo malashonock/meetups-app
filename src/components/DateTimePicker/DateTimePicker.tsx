631a173196d28d8efe904fad5e68fd282d1f88e7
Yan Malashonak <malashonock@gmail.com>
2023-01-06 20:25:38 +0300

    feat: hook DateTimePicker to formik

--- src/components/DateTimePicker/DateTimePicker.tsx
+++ src/components/DateTimePicker/DateTimePicker.tsx
@@ -1,41 +1,57 @@
-import { useEffect, useRef, useState } from 'react';
+import { Field, FieldProps } from 'formik';
+import { HTMLAttributes } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './DateTimePicker.scss';
 
-export const DateTimePicker = (): JSX.Element => {
-  const [date, setDate] = useState(new Date());
+type DateTimePickerProps = {
+  name: string;
+  placeholderText?: string;
+  // ...to be continued
+} & HTMLAttributes<HTMLDivElement>;
 
-  const handleChange = (date: Date) => setDate(date);
+export const DateTimePicker = ({
+  name,
+  placeholderText = 'd MMM yyyy HH:mm',
+  ...nativeHtmlAttributes
+}: DateTimePickerProps): JSX.Element => (
+  <Field name={name}>
+    {({ field: { name, value }, form: { setFieldValue } }: FieldProps) => {
+      const handleChange = (date: Date): void => setFieldValue(name, date);
 
-  const adjustTimeListHeight = () => {
-    const dayNamesHeight = document.querySelector<HTMLDivElement>(
-      '.react-datepicker__day-names',
-    )?.clientHeight;
-    const monthHeight = document.querySelector<HTMLDivElement>(
-      '.react-datepicker__month',
-    )?.clientHeight;
-    const timeList = document.querySelector<HTMLDivElement>(
-      '.react-datepicker__time-list',
-    );
+      const adjustTimeListHeight = () => {
+        const dayNamesHeight = document.querySelector<HTMLDivElement>(
+          '.react-datepicker__day-names',
+        )?.clientHeight;
+        const monthHeight = document.querySelector<HTMLDivElement>(
+          '.react-datepicker__month',
+        )?.clientHeight;
+        const timeList = document.querySelector<HTMLDivElement>(
+          '.react-datepicker__time-list',
+        );
 
-    if (timeList && dayNamesHeight && monthHeight) {
-      timeList.style.setProperty(
-        '--time-list-height',
-        `${dayNamesHeight + monthHeight}px`,
+        if (timeList && dayNamesHeight && monthHeight) {
+          timeList.style.setProperty(
+            '--time-list-height',
+            `${dayNamesHeight + monthHeight}px`,
+          );
+        }
+      };
+
+      return (
+        <div {...nativeHtmlAttributes}>
+          <DatePicker
+            name={name}
+            selected={value}
+            onChange={handleChange}
+            showTimeSelect
+            dateFormat="d MMM yyyy HH:mm"
+            timeFormat="HH:mm"
+            placeholderText={placeholderText}
+            onMonthChange={adjustTimeListHeight}
+          />
+        </div>
       );
-    }
-  };
-
-  return (
-    <DatePicker
-      selected={date}
-      onChange={handleChange}
-      showTimeSelect
-      dateFormat="d MMM yyyy HH:mm"
-      timeFormat="HH:mm"
-      placeholderText="d MMM yyyy HH:mm"
-      onMonthChange={adjustTimeListHeight}
-    ></DatePicker>
-  );
-};
+    }}
+  </Field>
+);
--- src/stories/DateTimePicker.stories.tsx
+++ src/stories/DateTimePicker.stories.tsx
@@ -1,13 +1,70 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { DateTimePicker } from 'components';
+import classNames from 'classnames';
+import {
+  Button,
+  DateTimePicker,
+  Typography,
+  TypographyComponent,
+} from 'components';
+import { Form, Formik } from 'formik';
+import * as yup from 'yup';
 
 export default {
   title: 'Components/DateTimePicker',
   component: DateTimePicker,
 } as ComponentMeta<typeof DateTimePicker>;
 
-const Template: ComponentStory<typeof DateTimePicker> = () => (
-  <DateTimePicker />
+interface FormValues {
+  [name: string]: Date | null;
+}
+
+const Template: ComponentStory<typeof DateTimePicker> = ({
+  name,
+  placeholderText,
+}) => (
+  <Formik<FormValues>
+    initialValues={{
+      [name]: null,
+    }}
+    validationSchema={yup.object().shape({
+      [name]: yup.date().required('Выберите дату и время'),
+    })}
+    onSubmit={(values: FormValues, { setSubmitting }) => {
+      console.log(values[name]);
+      setSubmitting(false); // onSubmit is sync, so need to call this
+    }}
+  >
+    {({ values }) => (
+      <Form
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          rowGap: '12px',
+        }}
+      >
+        <DateTimePicker name={name} placeholderText={placeholderText} />
+        <Button type="submit">Submit (check console logs)</Button>
+        <Typography
+          component={TypographyComponent.Paragraph}
+          className={classNames(
+            'font-family-primary',
+            'font-color-dark',
+            'font-size-xs',
+            'line-height-xs',
+          )}
+        >
+          Field value: {values[name]?.toISOString()}
+        </Typography>
+      </Form>
+    )}
+  </Formik>
 );
+
+export const Thumbnail = Template.bind({});
+Thumbnail.args = {
+  name: 'meetupDate',
+};
 
 export const Default = Template.bind({});
