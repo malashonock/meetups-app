d96686cc263bc64e3709a102c491ab0fde82386b
Yan Malashonak <malashonock@gmail.com>
2023-01-06 14:14:39 +0300

    refactor: convert Number extension method to plain function

--- src/helpers/number.ts
+++ src/helpers/number.ts
@@ -1,19 +1,7 @@
-// Inspired by these solutions:
-// https://stackoverflow.com/a/32229831
-// https://stackoverflow.com/a/32330345
+// Inspired by: https://stackoverflow.com/a/32229831
 
-declare global {
-  interface Number {
-    toFixedIfAny: (maxDecimalPlaces: number) => string;
-  }
-}
-
-Number.prototype.toFixedIfAny = function (
-  this: number,
-  maxDecimalPlaces: number,
-): string {
-  return +this.toFixed(maxDecimalPlaces) + '';
-};
+const toFixedIfAny = (num: number, maxDecimalPlaces: number): string =>
+  +num.toFixed(maxDecimalPlaces) + '';
 
 export const convertBytesToMb = (bytes: number): number => {
   const BYTES_IN_MEGABYTE = 1_000_000;
@@ -30,20 +18,20 @@ export const getFileSizeString = (
   maxDecimalPlaces: number = 0,
 ): string => {
   // start from most aggregating conversion
-  let result = convertBytesToMb(bytes).toFixedIfAny(maxDecimalPlaces);
+  let result = toFixedIfAny(convertBytesToMb(bytes), maxDecimalPlaces);
 
   if (+result > 0) {
     return `${result} Мб`;
   }
 
   // take less aggregating conversion
-  result = convertBytesToKb(bytes).toFixedIfAny(maxDecimalPlaces);
+  result = toFixedIfAny(convertBytesToKb(bytes), maxDecimalPlaces);
 
   if (+result > 0) {
     return `${result} Кб`;
   }
 
   // return without aggregation
-  result = bytes.toFixedIfAny(maxDecimalPlaces);
+  result = toFixedIfAny(bytes, maxDecimalPlaces);
   return `${result} байт`;
 };
