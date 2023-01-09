f91040d4646946ef6ae5db765619e06dd7c16f7c
Yan Malashonak <malashonock@gmail.com>
2023-01-09 22:52:02 +0300

    refactor: re-write TextField using InputField

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,73 +1,20 @@
-import { AllHTMLAttributes } from 'react';
-import { Field, FieldProps } from 'formik';
-
 import {
-  InputLabel,
-  HelperText,
-  TextInput,
-  InputFieldVariant,
+  InputFieldExternalProps,
+  InputField,
+  InputRenderProps,
 } from 'components';
 
-import styles from './TextField.module.scss';
-
-type TextFieldProps = {
-  name: string;
-  labelText: string;
-  placeholder?: string;
-  successText?: string;
-  helperText?: string;
-} & AllHTMLAttributes<HTMLElement>;
+type TextFieldProps = InputFieldExternalProps & {
+  placeholderText?: string;
+};
 
 export const TextField = ({
-  name,
-  labelText,
-  placeholder,
-  successText,
-  helperText,
-  ...nativeHtmlProps
+  placeholderText,
+  ...inputFieldProps
 }: TextFieldProps): JSX.Element => (
-  <Field name={name}>
-    {({ field, field: { value }, meta: { error, touched } }: FieldProps) => {
-      const hasError = !!error && !!touched;
-
-      let helperTextVariant = InputFieldVariant.Default;
-      if (hasError) {
-        helperTextVariant = InputFieldVariant.Error;
-      } else {
-        if (successText && !!value && !!touched) {
-          helperTextVariant = InputFieldVariant.Success;
-        }
-      }
-
-      const textInputVariant =
-        (hasError
-          ? InputFieldVariant.Error
-          : !!value && InputFieldVariant.Success) || InputFieldVariant.Default;
-
-      const finalHelperText = (
-        <>
-          {!!helperText && !touched
-            ? helperText
-            : hasError
-            ? error
-            : !!touched && successText}
-        </>
-      );
-
-      return (
-        <div {...nativeHtmlProps}>
-          <InputLabel name={name} className={styles.inputLabel}>
-            {labelText}
-          </InputLabel>
-          <TextInput
-            {...field}
-            className={styles.textInput}
-            variant={textInputVariant}
-            placeholder={placeholder}
-          />
-          <HelperText variant={helperTextVariant}>{finalHelperText}</HelperText>
-        </div>
-      );
-    }}
-  </Field>
+  <InputField {...inputFieldProps}>
+    {({ field, className }: InputRenderProps): JSX.Element => (
+      <input type="text" {...field} className={className} />
+    )}
+  </InputField>
 );
