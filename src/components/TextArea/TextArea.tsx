95303398ee689b5af875f92e61db6d296400451b
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-11 14:47:07 +0300

    feat: auto-size TextArea height

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,4 +1,4 @@
-import { AllHTMLAttributes } from 'react';
+import { AllHTMLAttributes, useEffect, useRef } from 'react';
 import classNames from 'classnames';
 
 import styles from './TextArea.module.scss';
@@ -11,21 +11,39 @@ export const TextArea = ({
   maxLetterCount,
   ...nativeHtmlProps
 }: TextAreaProps): JSX.Element => {
-  const letterCount = !!nativeHtmlProps.value
-    ? nativeHtmlProps.value.toString().length
-    : '0';
+  const { value, className } = nativeHtmlProps;
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  // Auto-adjust height of textarea
+  useEffect(() => {
+    if (textAreaRef.current) {
+      const { current: textArea } = textAreaRef;
+
+      // We need to reset the height momentarily to get the correct scrollHeight for the textarea
+      textArea.style.height = 'auto';
+      const scrollHeight = textArea.scrollHeight;
+
+      // We then set the height directly, outside of the render loop
+      // Trying to set this with state or a ref will product an incorrect value.
+      textArea.style.height = scrollHeight + 'px';
+    }
+  }, [textAreaRef.current, value]);
+
+  const letterCount = value?.toString().length ?? 0;
 
   return (
     <div className={styles.container}>
       <textarea
         {...nativeHtmlProps}
+        ref={textAreaRef}
+        rows={1}
         maxLength={maxLetterCount}
         className={classNames(
-          nativeHtmlProps.className,
+          className,
           styles.textArea,
         )}
       ></textarea>
-      {!!maxLetterCount && (
+      {maxLetterCount && maxLetterCount > 0 && (
         <div className={styles.counter}>
           <span className={styles.letterCount}>{letterCount}</span> /{' '}
           {maxLetterCount}
