85cad945745d69ef0a053977de588f63cfe23d3c
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-12 17:37:57 +0300

    refactor: add ViewNewsPage to index.ts

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,4 +1,5 @@
 export * from './MeetupPage/MeetupPage';
 export * from './ViewMeetupPage/ViewMeetupPage';
 export * from './NewsPage/NewsPage';
+export * from './ViewNewsPage/ViewNewsPage';
 export * from './NotFoundPage/NotFoundPage';
