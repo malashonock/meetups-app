8810a57cc909592beec367379c3a0a066c86ef3e
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-16 01:29:31 +0300

    refactor: make data generation functions async

--- server/generateInitialData.mjs
+++ server/generateInitialData.mjs
@@ -2,11 +2,11 @@ import {fixedUsers, generateUsers} from './data/users.mjs';
 import {fixedMeetups, generateMeetups, generateShortUsers} from './data/meetups.mjs';
 import {fixedNews, generateNews} from './data/news.mjs';
 
-export const generateInitialData = () => {
+export const generateInitialData = async () => {
   const users = [...fixedUsers, ...generateUsers(100)];
   const meetups = [...fixedMeetups, ...generateMeetups(20, users)];
   const participants = generateShortUsers(meetups, users);
   const votedUsers = generateShortUsers(meetups, users);
-  const news = [...fixedNews, ...generateNews(10)]
+  const news = [...fixedNews, ...(await generateNews(10))]
   return { users, meetups, participants, votedUsers, news };
 };
