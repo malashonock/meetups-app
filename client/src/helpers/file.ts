63e07aea026f8ef36932a234841cec538ac974be
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-16 01:31:31 +0300

    feat: add explicit url arg to getFileWithUrl helper

--- client/src/helpers/file.ts
+++ client/src/helpers/file.ts
@@ -1,8 +1,8 @@
 import { FileWithUrl } from 'types';
 
-export const getFileWithUrl = (file: File): FileWithUrl => {
+export const getFileWithUrl = (file: File, url?: string): FileWithUrl => {
   const fileWithUrl: FileWithUrl = Object.assign(file, {
-    url: URL.createObjectURL(file),
+    url: url ?? URL.createObjectURL(file),
   });
   return fileWithUrl;
 }
