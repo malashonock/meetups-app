a117d33bbf5e14964d4321609a955db2e47c8c2d
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-18 22:48:38 +0300

    refactor: Stepper stories

--- client/src/stories/Stepper.stories.tsx
+++ client/src/stories/Stepper.stories.tsx
@@ -1,35 +1,143 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { StepElementProps, Stepper } from 'components';
+import { Button, ButtonVariant, Stepper, StepperContext, Typography, TypographyComponent } from 'components';
+import { withRouter } from 'storybook-addon-react-router-v6';
 
 /* Mocked steps */
 const steps = [
   {
     title: 'Step 1',
-    element: ({ setConfirmed, index }: StepElementProps) => (
-      <div>
+    render: ({
+      stepsState,
+      setStepPassed,
+      activeStep,
+      handleBack,
+      handleNextStep,
+    }: StepperContext<{}>): JSX.Element => (
+      <div style={{ width: '100%', display: 'flex', flexDirection: 'column', rowGap: '12px' }}>
         <p>Some content of step 1</p>
-        <button onClick={() => setConfirmed(index, true)}>Click to pass</button>
-        <button onClick={() => setConfirmed(index, false)}>
-          Click to fail
-        </button>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button
+            variant={ButtonVariant.Secondary}
+            onClick={() => {
+              setStepPassed(activeStep.index, false);
+            }}
+            disabled={!activeStep.passed}
+          >
+            Click to fail
+          </Button>
+          <Button
+            variant={ButtonVariant.Primary}
+            onClick={() => {
+              setStepPassed(activeStep.index, true);
+            }}
+            disabled={activeStep.passed}
+          >
+            Click to pass
+          </Button>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button
+            variant={ButtonVariant.Secondary}
+            onClick={() => {
+              handleBack();
+            }}
+          >
+            Go back
+          </Button>
+          <Button
+            variant={ButtonVariant.Primary}
+            onClick={() => {
+              handleNextStep();
+            }}
+            disabled={!activeStep.passed}
+          >
+            Go to next step
+          </Button>
+        </div>
       </div>
     ),
   },
   {
     title: 'Step 2',
-    element: () => <p>This is a step without validation</p>,
-    noVerify: true,
+    render: ({
+      handlePreviousStep,
+      handleNextStep,
+    }: StepperContext<{}>): JSX.Element => (
+      <div style={{ width: '100%', display: 'flex', flexDirection: 'column', rowGap: '12px' }}>
+        <p>This is a step without validation</p>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button
+            variant={ButtonVariant.Secondary}
+            onClick={() => {
+              handlePreviousStep();
+            }}
+          >
+            Go to previous step
+          </Button>
+          <Button
+            variant={ButtonVariant.Primary}
+            onClick={() => {
+              handleNextStep();
+            }}
+          >
+            Go to next step
+          </Button>
+        </div>
+      </div>
+    ),
+    noValidate: true,
   },
   {
     title: 'Step 3',
-    element: ({ setConfirmed, index }: StepElementProps) => (
-      <div>
+    render: ({
+      stepsState,
+      setStepPassed,
+      activeStep,
+      handlePreviousStep,
+      handleFinish,
+    }: StepperContext<{}>): JSX.Element => (
+      <div style={{ width: '100%', display: 'flex', flexDirection: 'column', rowGap: '12px' }}>
         <p>Some content of step 3</p>
-        <button onClick={() => setConfirmed(index, true)}>Click to pass</button>
-        <button onClick={() => setConfirmed(index, false)}>
-          Click to fail
-        </button>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button
+            variant={ButtonVariant.Secondary}
+            onClick={() => {
+              setStepPassed(activeStep.index, false);
+            }}
+            disabled={!activeStep.passed}
+          >
+            Click to fail
+          </Button>
+          <Button
+            variant={ButtonVariant.Primary}
+            onClick={() => {
+              setStepPassed(activeStep.index, true);
+            }}
+            disabled={activeStep.passed}
+          >
+            Click to pass
+          </Button>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button
+            variant={ButtonVariant.Secondary}
+            onClick={() => {
+              handlePreviousStep();
+            }}
+          >
+            Go to previous step
+          </Button>
+          <Button
+            variant={ButtonVariant.Primary}
+            onClick={() => {
+              handleFinish();
+            }}
+            disabled={!activeStep.passed}
+          >
+            Go to finish
+          </Button>
+        </div>
       </div>
     ),
   },
@@ -38,6 +146,7 @@ const steps = [
 export default {
   title: 'Components/Stepper',
   component: Stepper,
+  decorators: [withRouter],
   parameters: {
     layout: 'centered',
   },
@@ -54,20 +163,3 @@ const Template: ComponentStory<typeof Stepper> = (args) => (
 );
 
 export const Default = Template.bind({});
-
-export const WithCustomButton = Template.bind({});
-
-WithCustomButton.args = {
-  finishButtonContent: 'Подтвердить',
-};
-
-export const WithCustomComplexButton = Template.bind({});
-
-WithCustomComplexButton.args = {
-  finishButtonContent: (
-    <div style={{ display: 'flex', columnGap: '10px' }}>
-      <span>+</span>
-      <span>Добавить</span>
-    </div>
-  ),
-};
