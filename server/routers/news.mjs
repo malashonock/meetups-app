1c15cc66f3b7db01375f0ef1e6c3eac3fc2ccd95
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-18 22:43:14 +0300

    refactor: move multer upload to middleware folder

--- server/routers/news.mjs
+++ server/routers/news.mjs
@@ -2,38 +2,8 @@ import express from 'express';
 import faker from 'faker';
 import { ensureAuthenticated } from '../middleware/ensureAthenticated.mjs';
 import { isModerator } from '../middleware/isModerator.mjs';
-import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
+import { upload } from '../middleware/upload.mjs';
 import { getUrlFromPublicPath } from '../utils.mjs';
-import { PUBLIC_DIR } from '../constants.mjs';
-
-const UPLOAD_DIR = path.join(PUBLIC_DIR, 'assets', 'images');
-
-const storage = multer.diskStorage({
-  destination: UPLOAD_DIR,
-  filename: (req, file, cb) => {
-    let fileName = file.originalname === 'blob'
-      ? faker.datatype.uuid()
-      : file.originalname;
-
-    cb(null, fileName);
-  },
-});
-
-const upload = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    const fileExists = fs.existsSync(path.join(UPLOAD_DIR, file.originalname));
-
-    if (fileExists) {
-      cb(null, false); // skip saving file
-      return;
-    }
-
-  cb(null, true); // save file
-  }
-})
 
 export const newsRoutes = (db) => {
   const newsRouter = express.Router();
