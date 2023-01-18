e74091fe5c78c1c871c1b05d639278ac6c38b6f7
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-19 01:34:17 +0300

    fix: remove fileFilter from multer config

--- server/middleware/upload.mjs
+++ server/middleware/upload.mjs
@@ -1,7 +1,6 @@
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
-import { getUrlFromPublicPath } from '../utils.mjs';
 import { PUBLIC_DIR } from '../constants.mjs';
 
 const UPLOAD_DIR = path.join(PUBLIC_DIR, 'assets', 'images');
@@ -19,14 +18,4 @@ const storage = multer.diskStorage({
 
 export const upload = multer({
   storage,
-  fileFilter: (req, file, cb) => {
-    const fileExists = fs.existsSync(path.join(UPLOAD_DIR, file.originalname));
-
-    if (fileExists) {
-      cb(null, false); // skip saving file
-      return;
-    }
-
-  cb(null, true); // save file
-  }
 });
