01a187a1cc21a586af6f2892544ffe96fea67eeb
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-22 15:00:22 +0300

    feat: implement user service

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,6 @@
 export * from './services/login.service';
 export * from './services/meetup.service';
 export * from './services/news.service';
-export * from './services/votedUsers.service';
+export * from './services/user.service';
 export * from './services/static.service';
 export * from './httpClient';
