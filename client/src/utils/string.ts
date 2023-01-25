1119692e4550bc0da8187e5260bda72d732abb44
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-26 01:20:41 +0300

    refactor: remove getInitials utility function

--- client/src/utils/string.ts
+++ client/src/utils/string.ts
@@ -1,9 +1,3 @@
 export const getFirstLetter = (text: string): string => {
   return text.length > 0 ? text[0] : '';
 };
-
-export const getInitials = (name: string, surname: string): string => {
-  return `${getFirstLetter(name)}${getFirstLetter(
-    surname,
-  )}`.toLocaleUpperCase();
-};
