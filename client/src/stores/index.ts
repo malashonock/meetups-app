ae861bbc2d5ab2d3e2b67bb55841a22aa258a94d
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-26 01:17:02 +0300

    feat: implement UserStore

--- client/src/stores/index.ts
+++ client/src/stores/index.ts
@@ -1,5 +1,6 @@
 export * from './root.store';
 export * from './auth.store';
+export * from './user.store';
 export * from './ui.store';
 export * from './meetup.store';
 export * from './news.store';
