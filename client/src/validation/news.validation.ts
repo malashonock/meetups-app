e7746648e0e10fdaeec26de11d4adf9af82d1f76
Yan Malashonak <yan.malashonak@gmail.com>
2023-01-30 02:54:35 +0300

    feat: integrate yup schemas with i18n

--- client/src/validation/news.validation.ts
+++ client/src/validation/news.validation.ts
@@ -1,6 +1,14 @@
 import * as yup from 'yup';
+import { i18n } from 'i18next';
 
-export const newsSchema = yup.object().shape({
-  title: yup.string().required('Введите заголовок новости'),
-  text: yup.string().required('Введите текст новости'),
-});
+export const newsSchema = ({ t }: i18n) =>
+  yup.object().shape({
+    title: yup
+      .string()
+      .required(
+        t('formFields.news.title.errorText') || 'News title is required',
+      ),
+    text: yup
+      .string()
+      .required(t('formFields.news.text.errorText') || 'News text is required'),
+  });
