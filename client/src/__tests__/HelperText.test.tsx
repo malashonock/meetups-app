c0d44fbea302d539cfc43e1091e9fcb8734c80c7
Yan Malashonak <yan.malashonak@gmail.com>
2023-02-09 12:44:22 +0300

    test: write unit test for HelperText component

---
+++ client/src/__tests__/HelperText.test.tsx
@@ -0,0 +1,30 @@
+import { screen, render } from '@testing-library/react';
+
+import { HelperText, InputFieldVariant } from 'components';
+
+describe('HelperText', () => {
+  it('renders text content', () => {
+    const handleClick = jest.fn();
+
+    render(<HelperText>Hint text</HelperText>);
+
+    const hintText = screen.getByText('Hint text');
+    expect(hintText).toBeInTheDocument();
+  });
+
+  it('can be assigned an external class or attribute', () => {
+    render(
+      <HelperText
+        variant={InputFieldVariant.Error}
+        className="test-class"
+        style={{ visibility: 'hidden' }}
+      >
+        Error message
+      </HelperText>,
+    );
+
+    const errorMessage = screen.queryByText('Error message');
+    expect(errorMessage).toBeInTheDocument();
+    expect(errorMessage).not.toBeVisible();
+  });
+});
