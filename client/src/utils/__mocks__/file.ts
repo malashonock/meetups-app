255a126e78c3cd02defacabb8c7f2c139d398f49
Yan Malashonak <yan.malashonak@gmail.com>
2023-02-10 11:52:40 +0300

    refactor: getFileWithUrl mock

--- client/src/utils/__mocks__/file.ts
+++ client/src/utils/__mocks__/file.ts
@@ -1,8 +1,6 @@
 import { FileWithUrl } from 'types';
 
-export const getFileWithUrl = (file: File, url?: string): FileWithUrl => {
-  const fileWithUrl: FileWithUrl = Object.assign(file, {
-    url: url ?? 'http://localhost/test-url',
-  });
-  return fileWithUrl;
-};
+export const getFileWithUrl = (file: File, url?: string): FileWithUrl => ({
+  ...file,
+  url: url ?? 'http://localhost/test-url',
+});
