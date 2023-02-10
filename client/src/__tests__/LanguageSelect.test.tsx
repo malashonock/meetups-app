f0eaaed180b55322ac2d32ea8f66b8a923110c86
Yan Malashonak <yan.malashonak@gmail.com>
2023-02-10 13:56:35 +0300

    test: write unit tests for LanguageSelect component

---
+++ client/src/__tests__/LanguageSelect.test.tsx
@@ -0,0 +1,45 @@
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { LanguageSelect } from 'components';
+import { Locale } from 'stores';
+import { useLocale } from 'hooks/useLocale';
+
+// Mock useLocale hook
+jest.mock('hooks/useLocale');
+const mockUseLocale = useLocale as jest.MockedFunction<typeof useLocale>;
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+describe('LanguageSelect', () => {
+  it('syncs selection with the current app locale', async () => {
+    mockUseLocale.mockReturnValue([Locale.RU, jest.fn()]);
+
+    render(<LanguageSelect />);
+
+    const selectedLanguage = screen.getByText('RU');
+    expect(selectedLanguage).toBeInTheDocument();
+  });
+
+  it('on language selection change, changes the app locale', async () => {
+    const mockSetLocale = jest.fn();
+
+    mockUseLocale.mockReturnValue([Locale.RU, mockSetLocale]);
+
+    render(<LanguageSelect />);
+
+    /* eslint-disable testing-library/no-node-access */
+    const languageSelect = screen
+      .getByTestId('language-select')
+      .querySelector('input') as HTMLInputElement;
+    userEvent.click(languageSelect);
+
+    const englishOption = await screen.findByText('EN');
+    expect(englishOption).toBeInTheDocument();
+
+    userEvent.click(englishOption);
+    expect(mockSetLocale).toHaveBeenCalledWith(Locale.EN);
+  });
+});
