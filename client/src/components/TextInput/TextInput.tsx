8ae0994f3980143ec70317c4eef6c2f5017313c7
Yan Malashonak <yan.malashonak@gmail.com>
2023-02-12 16:04:31 +0300

    test: add data-testid attribute

--- client/src/components/TextInput/TextInput.tsx
+++ client/src/components/TextInput/TextInput.tsx
@@ -3,5 +3,5 @@ import { HTMLAttributes } from 'react';
 export const TextInput = ({
   ...nativeHtmlProps
 }: HTMLAttributes<HTMLInputElement>): JSX.Element => (
-  <input type="text" {...nativeHtmlProps} />
+  <input type="text" {...nativeHtmlProps} data-testid="text-input" />
 );
