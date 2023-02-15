53c51a7fd4e08aeeee4840644a1f339c858e1476
Yan Malashonak <yan.malashonak@gmail.com>
2023-02-15 15:36:23 +0300

    fix: change getFileWithUrl  mock return type to File

--- client/src/utils/__mocks__/file.ts
+++ client/src/utils/__mocks__/file.ts
@@ -1,6 +1,11 @@
 import { FileWithUrl } from 'types';
 
-export const getFileWithUrl = (file: File, url?: string): FileWithUrl => ({
-  ...file,
-  url: url ?? `http://localhost/${file.name}`,
-});
+export const getFileWithUrl = (file: File, url?: string): FileWithUrl => {
+  Object.defineProperty(file, 'url', {
+    configurable: true,
+    enumerable: true,
+    writable: true,
+    value: url ?? `http://localhost/${file.name}`,
+  });
+  return file as FileWithUrl;
+};
