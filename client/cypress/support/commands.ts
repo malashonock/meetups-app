c9075a0b6b1f7a8b3ff68c8cf836fbd3a8d20f6b
Yan Malashonak <yan.malashonak@gmail.com>
2023-02-20 18:41:22 +0300

    test: add cypress createNewsArticle command

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import '@testing-library/cypress/add-commands';
+import { faker } from '@faker-js/faker';
 
 declare global {
   namespace Cypress {
@@ -8,6 +9,7 @@ declare global {
       login(name: string, surname: string, password: string): Chainable<void>;
       loginAsChief(): Chainable<void>;
       loginAsEmployee(): Chainable<void>;
+      createNewsArticle(): Chainable<string>;
     }
   }
 }
@@ -40,4 +42,39 @@ Cypress.Commands.add('loginAsChief', () => {
 
 Cypress.Commands.add('loginAsEmployee', () => {
   cy.login('employee', 'Gerlach', 'private');
+});
+
+Cypress.Commands.add('createNewsArticle', function () {
+  cy.loginAsChief();
+
+  const title: string = faker.company.catchPhrase();
+  const text: string = faker.lorem.paragraph();
+
+  const formData = new FormData();
+  formData.append('title', title);
+  formData.append('text', text);
+
+  cy.fixture<string>('../fixtures/test-image-1.jpeg', 'base64').then(
+    (image: string) => {
+      formData.append(
+        'image',
+        Cypress.Blob.base64StringToBlob(image, 'image/jpeg'),
+      );
+    },
+  );
+
+  cy.request({
+    url: 'http://localhost:8080/api/news',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    body: formData,
+  })
+    .its('body')
+    .then((body: ArrayBuffer) => {
+      const bodyAsString = Cypress.Blob.arrayBufferToBinaryString(body);
+      return JSON.parse(bodyAsString);
+    })
+    .its('id');
 });
