a1ab575fb7f21b2a0e9bfe539d90439d7229f71f
Yan Malashonak <yan.malashonak@gmail.com>
2023-02-21 00:28:24 +0300

    test: add e2e tests for meetup delete feature

--- client/cypress/e2e/meetup.delete.cy.ts
+++ client/cypress/e2e/meetup.delete.cy.ts
@@ -2,7 +2,7 @@ export {};
 
 describe('Delete meetup', () => {
   describe('given a user is logged in', () => {
-    it('should delete an existing meetup', function () {
+    it('should delete meetup via Delete button on meetup card', function () {
       cy.createTopic().then((createdMeetupId: string) => {
         cy.visit('/meetups/topics');
         cy.get(`[href="/meetups/${createdMeetupId}"]`, {
@@ -20,6 +20,32 @@ describe('Delete meetup', () => {
         }).should('not.exist');
       });
     });
+
+    it('should delete meetup via Delete button on view topic page', function () {
+      cy.createTopic().then((createdMeetupId: string) => {
+        cy.visit(`/meetups/${createdMeetupId}`);
+        cy.get('#btn-delete', {
+          timeout: 10_000,
+        }).click();
+
+        // Should redirect to topics
+        cy.url().should('contain', '/meetups/topics');
+
+        cy.visit(`/meetups/${createdMeetupId}`);
+        cy.get(`[href="/meetups/${createdMeetupId}"]`, {
+          timeout: 10_000,
+        }).should('not.exist');
+      });
+    });
+
+    it('should not show Delete button on view meetup page after topic approval', function () {
+      cy.createMeetupDraft().then((createdMeetupId: string) => {
+        cy.visit(`/meetups/${createdMeetupId}`);
+        cy.get('#btn-delete', {
+          timeout: 10_000,
+        }).should('not.exist');
+      });
+    });
   });
 
   describe('given no user is logged in', () => {
@@ -30,6 +56,14 @@ describe('Delete meetup', () => {
         .within(() => {
           cy.get('[data-testid="delete-button"]').should('not.exist');
         });
+    });
+
+    it('there should be no Delete button on the view topic page', () => {
+      cy.visit('/meetups/topics');
+      cy.get('[class*="MeetupCard"]', { timeout: 10_000 }).first().click();
+      cy.get('#btn-delete', {
+        timeout: 10_000,
+      }).should('not.exist');
     });
   });
 });
