1690e38a54f1cac747e1c6fb61a537b4400ba849
Yan Malashonak <yan.malashonak@gmail.com>
2023-02-22 15:35:22 +0300

    feat: leverage ILoader fields in MeetupTabContent

--- client/src/components/MeetupStagesTabs/MeetupTabContent/MeetupTabContent.tsx
+++ client/src/components/MeetupStagesTabs/MeetupTabContent/MeetupTabContent.tsx
@@ -7,9 +7,11 @@ import { useTranslation } from 'react-i18next';
 import {
   Button,
   ButtonVariant,
+  LoadingSpinner,
   MeetupCard,
   MeetupCardVariant,
 } from 'components';
+import { NotFoundPage } from 'pages';
 import { MeetupStatus } from 'model';
 import { Meetup } from 'stores';
 import { useMeetupStore } from 'hooks';
@@ -23,10 +25,9 @@ interface MeetupTabContentProps {
 
 export const MeetupTabContent = observer(
   ({ variant }: MeetupTabContentProps) => {
-    const { meetups } = useMeetupStore();
+    const { meetups, isLoading, isError } = useMeetupStore();
     const [selectedMeetups, setSelectedMeetups] = useState<Meetup[]>();
     const { t } = useTranslation();
-
     const navigate = useNavigate();
 
     const openCreateMeetupPage = () => navigate('/meetups/create');
@@ -65,33 +66,43 @@ export const MeetupTabContent = observer(
       }
     }, [variant, meetups, meetups?.length]);
 
+    if (!meetups || isError) {
+      return <NotFoundPage />;
+    }
+
     return (
       <section className={styles.meetupsTab}>
-        <div className={styles.wrapper}>
-          <div className={styles.counter}>
-            {t('meetupTabContent.meetupCount', {
-              context: variant,
-              count: selectedMeetups?.length || 0,
-            })}
-          </div>
-          {variant === MeetupCardVariant.Topic && (
-            <Button
-              id="btn-create-meetup"
-              variant={ButtonVariant.Secondary}
-              onClick={openCreateMeetupPage}
-              className={styles.createMeetupBtn}
-            >
-              {t('meetupTabContent.createMeetupBtn')}
-            </Button>
-          )}
-        </div>
-        <div className={styles.meetups}>
-          {selectedMeetups?.map((meetup) => (
-            <NavLink to={`/meetups/${meetup.id}`} key={meetup.id}>
-              <MeetupCard meetup={meetup} />
-            </NavLink>
-          ))}
-        </div>
+        {isLoading ? (
+          <LoadingSpinner text={t('loadingText.meetups')} />
+        ) : (
+          <>
+            <div className={styles.wrapper}>
+              <div className={styles.counter}>
+                {t('meetupTabContent.meetupCount', {
+                  context: variant,
+                  count: selectedMeetups?.length || 0,
+                })}
+              </div>
+              {variant === MeetupCardVariant.Topic && (
+                <Button
+                  id="btn-create-meetup"
+                  variant={ButtonVariant.Secondary}
+                  onClick={openCreateMeetupPage}
+                  className={styles.createMeetupBtn}
+                >
+                  {t('meetupTabContent.createMeetupBtn')}
+                </Button>
+              )}
+            </div>
+            <div className={styles.meetups}>
+              {selectedMeetups?.map((meetup) => (
+                <NavLink to={`/meetups/${meetup.id}`} key={meetup.id}>
+                  <MeetupCard meetup={meetup} />
+                </NavLink>
+              ))}
+            </div>
+          </>
+        )}
       </section>
     );
   },
