a8565b842de05fa30223043895dee24fbcef8ee7
Yan Malashonak <yan.malashonak@gmail.com>
2023-03-02 01:45:43 +0300

    feat: add alerts to useUiStore result

--- client/src/hooks/useUiStore.ts
+++ client/src/hooks/useUiStore.ts
@@ -2,15 +2,19 @@ import { useContext } from 'react';
 
 import { UiStore } from 'stores';
 import { RootContext } from 'components';
+import { Alert } from 'types';
 
 export interface UseUiStoreResult {
   uiStore?: UiStore;
+  alerts?: Alert[];
 }
 
 export const useUiStore = (): UseUiStoreResult => {
   const uiStore = useContext(RootContext)?.uiStore;
+  const alerts = uiStore?.alerts;
 
   return {
     uiStore,
+    alerts,
   };
 };
