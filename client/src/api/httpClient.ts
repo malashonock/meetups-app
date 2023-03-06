ffdf8a3bdd63cecf29c343c26bd215e542f88fc9
Yan Malashonak <yan.malashonak@gmail.com>
2023-03-06 18:04:42 +0300

    test: write unit tests for axios custom error interceptor

--- client/src/api/httpClient.ts
+++ client/src/api/httpClient.ts
@@ -16,28 +16,30 @@ export const httpClient = axios.create({
   withCredentials: true,
 });
 
+export const errorInterceptor = (error: AxiosError) => {
+  if (error.code === 'ERR_NETWORK') {
+    throw new NetworkError();
+  }
+
+  switch (error.response?.status) {
+    case 401:
+      throw new NotAuthenticatedError();
+    case 403:
+      throw new NotAuthorizedError();
+    case 404:
+      throw new NotFoundError();
+    case 500:
+      throw new ServerError();
+    default:
+      throw new AppError(
+        error.response?.status.toString() || error.code || 'UNKNOWN_ERROR',
+        error.name,
+        error.message,
+      );
+  }
+};
+
 httpClient.interceptors.response.use(
   (response: AxiosResponse) => response,
-  (error: AxiosError) => {
-    if (error.code === 'ERR_NETWORK') {
-      throw new NetworkError();
-    }
-
-    switch (error.response?.status) {
-      case 401:
-        throw new NotAuthenticatedError();
-      case 403:
-        throw new NotAuthorizedError();
-      case 404:
-        throw new NotFoundError();
-      case 500:
-        throw new ServerError();
-      default:
-        throw new AppError(
-          error.response?.status.toString() || error.code || 'UNKNOWN_ERROR',
-          error.name,
-          error.message,
-        );
-    }
-  },
+  errorInterceptor,
 );
