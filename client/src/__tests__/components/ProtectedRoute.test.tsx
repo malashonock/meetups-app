7f6771a8f7ec726b3b2289b1faebe4b8c75e912c
Yan Malashonak <yan.malashonak@gmail.com>
2023-03-10 18:15:01 +0300

    test: update unit tests for ProtectedRoute component

--- client/src/__tests__/components/ProtectedRoute.test.tsx
+++ client/src/__tests__/components/ProtectedRoute.test.tsx
@@ -2,9 +2,11 @@ import { PropsWithChildren } from 'react';
 import { screen, render } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
-import { ProtectedRoute, RedirectCondition } from 'components';
-import { mockFullUser } from 'model/__fakes__';
+import { ProtectedRoute, RedirectCondition, RootContext } from 'components';
+import { mockFullUser, mockFullUser2 } from 'model/__fakes__';
 import { useAuthStore } from 'hooks';
+import { RootStore } from 'stores';
+import { AlertSeverity } from 'types';
 
 // Mock useAuthStore hook
 jest.mock('hooks', () => {
@@ -24,6 +26,10 @@ beforeEach(() => {
   });
 });
 
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
 const MockRouter = ({ children }: PropsWithChildren): JSX.Element => (
   <MemoryRouter initialEntries={['/protected']}>
     <Routes>
@@ -34,6 +40,18 @@ const MockRouter = ({ children }: PropsWithChildren): JSX.Element => (
       <Route path="/protected" element={children} />
     </Routes>
   </MemoryRouter>
+);
+
+const mockRootStore = new RootStore();
+const spiedOnRootStoreOnAlert = jest.fn();
+mockRootStore.onAlert = spiedOnRootStoreOnAlert;
+
+const MockRouterWithRootStoreProvider = ({
+  children,
+}: PropsWithChildren): JSX.Element => (
+  <RootContext.Provider value={mockRootStore}>
+    <MockRouter>{children}</MockRouter>
+  </RootContext.Provider>
 );
 
 const ProtectedPage = (): JSX.Element => <h1>Protected page</h1>;
@@ -92,6 +110,160 @@ describe('ProtectedRoute', () => {
 
         const newsPage = screen.queryByText('News page');
         expect(newsPage).toBeInTheDocument();
+      });
+
+      it('should push an error alert to the root store alerts', () => {
+        render(
+          <ProtectedRoute>
+            <ProtectedPage />
+          </ProtectedRoute>,
+          {
+            wrapper: MockRouterWithRootStoreProvider,
+          },
+        );
+
+        expect(spiedOnRootStoreOnAlert).toHaveBeenCalledTimes(1);
+        expect(spiedOnRootStoreOnAlert.mock.calls[0][0].severity).toBe(
+          AlertSeverity.Error,
+        );
+      });
+    });
+  });
+
+  describe('if redirectIf is set to "nonAdmin"', () => {
+    it('if a user with admin rights is logged in, navigates to the protected route', () => {
+      mockUseAuthStore.mockReturnValue({
+        loggedUser: mockFullUser,
+        isInitialized: true,
+      });
+
+      render(
+        <ProtectedRoute redirectIf={RedirectCondition.NonAdmin}>
+          <ProtectedPage />
+        </ProtectedRoute>,
+        {
+          wrapper: MockRouter,
+        },
+      );
+
+      const protectedPage = screen.queryByText('Protected page');
+      expect(protectedPage).toBeInTheDocument();
+    });
+
+    describe('if a user with non-admin-rights is logged in', () => {
+      beforeEach(() => {
+        mockUseAuthStore.mockReturnValue({
+          loggedUser: mockFullUser2,
+          isInitialized: true,
+        });
+      });
+
+      it('if redirectTo is omitted, redirects to login page', () => {
+        render(
+          <ProtectedRoute redirectIf={RedirectCondition.NonAdmin}>
+            <ProtectedPage />
+          </ProtectedRoute>,
+          {
+            wrapper: MockRouter,
+          },
+        );
+
+        const protectedPage = screen.queryByText('Protected page');
+        expect(protectedPage).toBeNull();
+
+        const loginPage = screen.queryByText('Login page');
+        expect(loginPage).toBeInTheDocument();
+      });
+
+      it('if redirectTo is specified, redirects to the specified route', () => {
+        render(
+          <ProtectedRoute
+            redirectIf={RedirectCondition.NonAdmin}
+            redirectTo="/news"
+          >
+            <ProtectedPage />
+          </ProtectedRoute>,
+          {
+            wrapper: MockRouter,
+          },
+        );
+
+        const protectedPage = screen.queryByText('Protected page');
+        expect(protectedPage).toBeNull();
+
+        const newsPage = screen.queryByText('News page');
+        expect(newsPage).toBeInTheDocument();
+      });
+
+      it('should push an error alert to the root store alerts', () => {
+        render(
+          <ProtectedRoute redirectIf={RedirectCondition.NonAdmin}>
+            <ProtectedPage />
+          </ProtectedRoute>,
+          {
+            wrapper: MockRouterWithRootStoreProvider,
+          },
+        );
+
+        expect(spiedOnRootStoreOnAlert).toHaveBeenCalledTimes(1);
+        expect(spiedOnRootStoreOnAlert.mock.calls[0][0].severity).toBe(
+          AlertSeverity.Error,
+        );
+      });
+    });
+
+    describe('if no user is logged in', () => {
+      it('if redirectTo is omitted, redirects to login page', () => {
+        render(
+          <ProtectedRoute redirectIf={RedirectCondition.NonAdmin}>
+            <ProtectedPage />
+          </ProtectedRoute>,
+          {
+            wrapper: MockRouter,
+          },
+        );
+
+        const protectedPage = screen.queryByText('Protected page');
+        expect(protectedPage).toBeNull();
+
+        const loginPage = screen.queryByText('Login page');
+        expect(loginPage).toBeInTheDocument();
+      });
+
+      it('if redirectTo is specified, redirects to the specified route', () => {
+        render(
+          <ProtectedRoute
+            redirectIf={RedirectCondition.NonAdmin}
+            redirectTo="/news"
+          >
+            <ProtectedPage />
+          </ProtectedRoute>,
+          {
+            wrapper: MockRouter,
+          },
+        );
+
+        const protectedPage = screen.queryByText('Protected page');
+        expect(protectedPage).toBeNull();
+
+        const newsPage = screen.queryByText('News page');
+        expect(newsPage).toBeInTheDocument();
+      });
+
+      it('should push an error alert to the root store alerts', () => {
+        render(
+          <ProtectedRoute redirectIf={RedirectCondition.NonAdmin}>
+            <ProtectedPage />
+          </ProtectedRoute>,
+          {
+            wrapper: MockRouterWithRootStoreProvider,
+          },
+        );
+
+        expect(spiedOnRootStoreOnAlert).toHaveBeenCalledTimes(1);
+        expect(spiedOnRootStoreOnAlert.mock.calls[0][0].severity).toBe(
+          AlertSeverity.Error,
+        );
       });
     });
   });
@@ -155,6 +327,26 @@ describe('ProtectedRoute', () => {
         const newsPage = screen.queryByText('News page');
         expect(newsPage).toBeInTheDocument();
       });
+    });
+  });
+
+  describe('given authStore is not initialized', () => {
+    it('should render a loading spinner', () => {
+      mockUseAuthStore.mockReturnValue({
+        loggedUser: null,
+        isInitialized: false,
+      });
+
+      render(
+        <ProtectedRoute>
+          <ProtectedPage />
+        </ProtectedRoute>,
+        {
+          wrapper: MockRouter,
+        },
+      );
+
+      expect(screen.getByText('loadingText.default')).toBeInTheDocument();
     });
   });
 });
